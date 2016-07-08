0314f295633e164aaff6d155268cfbb7d55233dc
raVen <raVen>
2016-07-08 01:55:08 -0400

    removed 3d and profile html

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,7 +2,19 @@
 
 angular.module('personalSiteApp.profile', ['ngRoute'])
 .controller('ProfileController', ["$scope", "$http", function($scope, $http) {
-    $http.get('data/profileData.json').success(function(response){
-        $scope.profileData = response;
-    });
+    $http.get('https://us.api.battle.net/wow/guild/Tichondrius/Seriously%20Brutal?fields=members&locale=en_US&apikey=u2d8h5gkuwtvkegr3wxzcqct85uq48z9')
+    .then(
+    	function(response){
+    		$scope.profileData = {};
+	        $scope.profileData.name = response.data.name;
+	        $scope.profileData.email = response.data.achievementPoints;
+	        console.log("SUCCESS");
+	        console.log(response);
+	    },
+	    function(response){
+	        //$scope.profileData = response;
+	        console.log("FAILED");
+	        console.log(response);
+	    }
+	);
 }]);
